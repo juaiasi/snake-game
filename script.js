ac4af4b2b8772f9e06d6b55a4c46ed2396696d00
juaiasi <juaiasi@gmail.com>
2021-07-28 09:05:17 -0300

    Cobrinha se move com as setas

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -18,5 +19,52 @@ function criarCobra(){
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
-criarBG();
-criarCobra();
+
+document.addEventListener('keydown', update);
+
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direcstion = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function criarApple(){
+    function generateRandomFloatInRange(max) {
+        return Math.floor(Math.random() * max);
+    }
+    let applePosition = [];
+    applePosition[0] = generateRandomFloatInRange(15)*box;
+    applePosition[1] = generateRandomFloatInRange(15)*box;
+    context.fillStyle = "red";
+    context.fillRect(applePosition[0], applePosition[1], box, box);
+}
+
+criarApple();
+
+function iniciarJogo(){
+    criarBG();
+    criarCobra();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
+
+
+
